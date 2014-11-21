076c37bac501b7f4384eea7ac92d8f752ab4b715
mfrancaleal <mfrancaleal@gmail.com>
2014-11-21 01:42:14 -0300

    21112014
    Configuração e Algumas Classes

---
+++ src/com/myimage/controller/UsuarioController.java
@@ -0,0 +1,54 @@
+package com.myimage.controller;
+ 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import com.myimage.model.Usuario;
+ 
+@WebServlet("/UsuarioController")
+public class UsuarioController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+ 
+    public UsuarioController() {
+        super();
+    }
+ 
+    private void processarRequisicao(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException {
+ 
+        String action = request.getParameter("action");
+ 
+        if (action == null) {
+            throw new ServletException("No action specified.");
+        } else if (action.equals("criar_conta")) {
+            criarConta(request, response);
+        }
+    }
+ 
+    private void criarConta(HttpServletRequest request,
+            HttpServletResponse response) {
+ 
+        String email = request.getParameter("email");
+        String nome = request.getParameter("nome");
+        String senha = request.getParameter("senha");
+ 
+        Usuario usuario = new Usuario();
+        usuario.setEmail(email);
+        usuario.setNome(nome);
+        usuario.setSenha(senha);
+    }
+ 
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        processarRequisicao(request, response);
+    }
+ 
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        processarRequisicao(request, response);
+    }
+}
